57635194f0da461c21e4b0547dc82f41eb466393
Ri8x <42354272+Ri8x@users.noreply.github.com>
2026-09-16 16:11:55 +0100

    release: point the Homebrew formula at v0.1.1

--- Formula/adb-qr.rb
+++ Formula/adb-qr.rb
@@ -1,9 +1,9 @@
 class AdbQr < Formula
   desc "Pair Android devices over wireless ADB using a QR code"
   homepage "https://github.com/Ri8x/adb-qr"
-  url "https://github.com/Ri8x/adb-qr/releases/download/v0.1.0/adb-qr-v0.1.0-macos-universal.tar.gz"
-  version "0.1.0"
-  sha256 "feed1ac05994cfbaad3b3f10590b479b7fb7c526a8bf1029f5061af7b8088607"
+  url "https://github.com/Ri8x/adb-qr/releases/download/v0.1.1/adb-qr-v0.1.1-macos-universal.tar.gz"
+  version "0.1.1"
+  sha256 "0a11f943a1cdf4bbfd7eaa72115e9767b67079859b757996fd454290232ef571"
   license "MIT"
 
   depends_on macos: :big_sur
